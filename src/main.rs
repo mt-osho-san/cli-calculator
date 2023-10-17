dc4dc7e0a638d196d4d196083924a58ddd6b08d6
少路拓洋 <shoji@shojis-macbook.local>
2023-10-17 22:49:00 +0900

    change main.rs
    add unit test

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,39 @@ fn main() {
         operation,
     } = CommandLineArgs::from_args();
     
-    let x = match operation {
+    let result = match operation {
         Add { num1, num2 } => calc::add(num1, num2),
         Sub { num1, num2 } => calc::sub(num1, num2),
         Mul { num1, num2 } => calc::mul(num1, num2),
         Div { num1, num2 } => calc::div(num1, num2),
     };
-    match x {
+
+    match result {
         Ok(v) => println!("The answer is {}", v),
         Err(e) => eprintln!("Error: {}", e),
     }
+}
+
+#[test]
+fn add_two_numbers() {
+    let result = calc::add(2_f64,3_f64);
+    assert_eq!(result, Ok(5_f64));
+}
+
+#[test]
+fn sub_two_numbers() {
+    let result = calc::sub(3.0, 2.0);
+    assert_eq!(result, Ok(1.0));
+}
+
+#[test]
+fn mul_two_numbers() {
+    let result = calc::mul(3.0, 2.0);
+    assert_eq!(result, Ok(6.0));
+}
+
+#[test]
+fn div_two_numbers() {
+    let result = calc::div(3.0, 2.0);
+    assert_eq!(result, Ok(1.5));
 }
